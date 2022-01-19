e09d4a0b5b702a78bfa3da91b9cf6be2e6254643
Anna Kulieshova <unk2cl@gmail.com>
2022-01-19 14:40:58 +0200

    Add assets

--- MyPlaces/MainViewController.swift
+++ MyPlaces/MainViewController.swift
@@ -11,7 +11,7 @@ class MainViewController: UITableViewController {
     
     let restaurantNames = [
         "Burger Heroes", "Kitchen", "Bonsai", "Дастархан",
-        "Индокитай", "X.O.", "Балкан Гриль", "Sherlock Holmes",
+        "Индокитай", "X.O", "Балкан Гриль", "Sherlock Holmes",
         "Speak Easy", "Morris Pub", "Вкусные истории",
         "Классик", "Love&Life", "Шок", "Бочка"
     ]
@@ -30,6 +30,7 @@ class MainViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
         cell.textLabel?.text = restaurantNames[indexPath.row]
+        cell.imageView?.image = UIImage(named: restaurantNames[indexPath.row])
 
         return cell
     }
